14c1e05a388a0f5f0e4f07c7753b122be948fbc0
MilleNRasmussen <millenrasmussen@gmail.com>
2026-03-01 00:09:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ async def flic_webhook(request: Request):
 
         cur.execute(
             "INSERT INTO ButtonLog (ButtonId) VALUES (%s)",
-            (button-serial-number,)
+            (ButtonId,)
         )
 
         conn.commit()
