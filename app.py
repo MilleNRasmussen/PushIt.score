30b1fe7ddf8134212dd832d5d80a6f474375843a
MilleNRasmussen <millenrasmussen@gmail.com>
2026-03-13 23:29:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def pause_inactive_matches():
             SET mh.Status='SystemPaused',
                 PausedAt = NOW()
             WHERE mh.Status='Live'
-            AND COALESCE(md.LastPoint, mh.StartedAt) < NOW() - INTERVAL 20 MINUTE
+            AND COALESCE(md.LastPoint, mh.StartedAt) < NOW() - INTERVAL 10 MINUTE
         """)
 
         conn.commit()
