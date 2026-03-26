d94de69c65ac912a0da28c30b81803afb6310380
MilleNRasmussen <millenrasmussen@gmail.com>
2026-03-26 20:07:26 +0100

    Change interval from 10 minutes to 1 minute

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def pause_inactive_matches():
             SET mh.Status='SystemPaused',
                 PausedAt = NOW()
             WHERE mh.Status='Live'
-            AND COALESCE(md.LastPoint, mh.StartedAt) < NOW() - INTERVAL 10 MINUTE
+            AND COALESCE(md.LastPoint, mh.StartedAt) < NOW() - INTERVAL 1 MINUTE
         """)
 
         conn.commit()
