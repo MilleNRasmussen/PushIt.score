73709434507adc8ab241d0a39aceec52bbd68a57
MilleNRasmussen <millenrasmussen@gmail.com>
2026-03-30 21:47:10 +0200

    Fix import formatting for WebSocket in app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi.responses import StreamingResponse
 from fastapi.responses import JSONResponse
-from fastapi import WebSocket
+from fastapi import WebSocket 
 
 app = FastAPI()
 
