a9753fdfc241f951f6d6e55a5e2c7dcfaedaaf6e
MilleNRasmussen <millenrasmussen@gmail.com>
2026-04-11 21:40:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -840,13 +840,15 @@ async def webhook_delete_point(request: Request):
     try:
         print("ENDPOINT: DELETE", flush=True)
 
-        print("MATCH ID:", data["match_id"], flush=True)
+        
 
         button_id = request.headers.get("button-serial-number")
         if not button_id:
             return {"error": "No button id"}
 
         data = get_match_data(cur, button_id)
+
+        print("MATCH ID:", data["match_id"], flush=True)
 
         if not data:
             broadcast_flic(button_id)
